116a802e7b03796db6f7ebb5c6b1cf768c2cf717
AjayMunagala <88882208+AjayMunagala@users.noreply.github.com>
2025-06-17 10:01:51 +0000

    Ajay

--- com.Java/src/FunctionPrg.java
+++ com.Java/src/FunctionPrg.java
@@ -1,18 +1,35 @@
 
 
-@FunctionalInterface
-interface EvenOdd{
-    void display(int a);
-}
-class Main{
+// @FunctionalInterface
+// interface EvenOdd{
+//     void display(int a);
+// }
+// class Main{
+//     public static void main(String[] args){
+//     EvenOdd ev =(n)->{
+//         if(n%2 ==0){
+//             System.out.println("even");
+//         }else{
+//             System.out.println("odd");
+//         }
+//     };
+//     ev.display(2);
+// }
+// }
+//----------------------------------------------------------------------------------------
+
+import java.util.function.Consumer;
+
+class FunctionalInterface{
     public static void main(String[] args){
-    EvenOdd ev =(n)->{
-        if(n%2 ==0){
-            System.out.println("even");
-        }else{
-            System.out.println("odd");
-        }
-    };
-    ev.display(2);
-}
+        Consumer<Integer> c1 = (value)->System.out.println(value);
+        Consumer<Integer> c2 = System.out::println;
+        Consumer<String> c3 =  (value)->System.out.print(value);
+        Consumer<String>  c4 = System.out::println;
+        c1.accept(10);
+        c2.accept(20);
+        c3.accept("AJay");
+        c4.accept("Kumar");
+
+    }
 }
---
+++ javap/Add.java
@@ -0,0 +1,197 @@
+package javap;
+
+/*
+relation operators
+=================
+==
+!=
+>
+<
+>=
+<=
+
+
+//Assignment operators
+======================
+=
++=                       c = c-a      c -= a;
+-=
+*=
+/=
+%=
+
+
+bitwise opertors
+==============
+&
+^
+~
+<<
+>>
+keywords68
+========
+reserved keywords---51
+contextual keywords-- 17
+
+
+
+class
+======
+synatx
+class <class name>
+
+class <name>{
+var
+method
+blcks
+construc
+ineerclass
+}
+class Name upper case
+class name meanging 
+ class doesnot begin special characters
+   may begin with _ or $
+
+   int a = 10000;
+   int a  = 10,000; java not allowed
+   int a = 10_000;  allowed
+   Name 
+   _name
+   $name
+   Apple456
+classs doenot contain special symbol
+class may contain digits
+keyword cannot be used as clas name
+class name doesnot contain space
+
+class A{
+
+}
+DataTYpes & variables
+primitive 
+non primitive(referenced)
+
+byte 
+short
+int 
+long
+
+float
+double
+char
+boolean
+=====================
+variable
+
+byte glass = 1;
+
+variable
+
+<datatype> <variablename>
+int a;
+datatype g = 1;
+
+
+
+variable :
+===========
+1)local variable
+2)global variable
+    1)static
+    2)instance varible(Non-static)
+
+
+    class{
+    method{}
+    const{}
+    block;
+    }
+    
+    int -0
+    byte  0
+    long  0
+    short 0
+    float 0.0
+    double 0.0
+    char  
+    boolean false;
+    string null    
+    
+
+    global variable
+    ==============
+    1)outside method ,constructor
+    2)default values
+    3)output values
+
+
+
+    static varible:
+    ===============
+    instance variable
+    =================
+
+
+    control flow statements
+    =======================
+    3 categories
+    1)decision making statements
+    2)LOOPING STATEMENTS
+    3)branching
+
+    1)Decision making statements
+    ==========================
+    if
+    if - else
+    if - elseif
+    switch
+
+              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+class Comparison{
+  public static void main(String[] args) {
+    int a;
+    int b;
+
+    
+
+    
+  }
+}
